ee47696d59342f60231c96653ab75f788c95d2bc
Mohsen <m.khasheii@gmail.com>
2022-09-18 03:07:10 +0430

    update decorator

--- Decorator/IceCream.js
+++ Decorator/IceCream.js
@@ -1,27 +1,26 @@
-class IceCream { 
-    constructor(flavor) {
-        this.flavor = flavor
-    }
-
-    describe() {
-        console.log("Normal ice cream,", this.flavor, " flavored")
-    }
+class IceCream {
+	constructor(flavor) {
+		this.flavor = flavor;
+	}
+	describe() {
+		console.log('Normal ice cream,', this.flavor, ' flavored');
+	}
 }
 
 function decorateWith(object, decoration) {
-    object.decoration = decoration
-    let oldDescr = object.describe //saving the reference to the method so we can use it later
-    object.describe = function() {
-        oldDescr.apply(object)
-        console.log("With extra", this.decoration)
-    }
-    return object
+	object.decoration = decoration;
+	let oldDescr = object.describe; //saving the reference to the method so we can use it later
+	object.describe = function () {
+		oldDescr.apply(object);
+		console.log('With extra', this.decoration);
+	};
+	return object;
 }
 
 // usage
 
-let oIce = new IceCream("vanilla") //A normal vanilla flavored ice cream...
-oIce.describe() 
+let oIce = new IceCream('vanilla'); //A normal vanilla flavored ice cream...
+oIce.describe();
 
-let vanillaWithNuts = decorateWith(oIce, "nuts") //... and now we add some nuts on top of it
-vanillaWithNuts.describe()
+let vanillaWithNuts = decorateWith(oIce, 'nuts'); //... and now we add some nuts on top of it
+vanillaWithNuts.describe();
--- Decorator/Pizza.js
+++ Decorator/Pizza.js
@@ -1,31 +1,26 @@
 class Pizza {
-    constructor() {
-        this.base_price = 10
-    }
-    calculatePrice() {
-        return this.base_price
-    }
+	constructor() {
+		this.base_price = 10;
+	}
+	calculatePrice() {
+		return this.base_price;
+	}
 }
 
 function addTopping(pizza, topping, price) {
-
-    let prevMethod = pizza.calculatePrice
-    pizza.toppings = [...(pizza.toppings || []), topping]
-    pizza.calculatePrice = function() {
-        return price + prevMethod.apply(pizza)
-    }
-    return pizza
+	let prevMethod = pizza.calculatePrice;
+	pizza.toppings = [...(pizza.toppings || []), topping];
+	pizza.calculatePrice = function () {
+		return price + prevMethod.apply(pizza);
+	};
+	return pizza;
 }
 
 // usage
 
-let oPizza = new Pizza()
+let oPizza = new Pizza();
 
-oPizza = addTopping(
-            addTopping(
-                oPizza, "muzzarella", 10
-            ), "anana", 100
-        )
+oPizza = addTopping(addTopping(oPizza, 'muzzarella', 10), 'anana', 100);
 
-console.log("Toppings: ", oPizza.toppings.join(", "))
-console.log("Total price: ", oPizza.calculatePrice())
+console.log('Toppings: ', oPizza.toppings.join(', '));
+console.log('Total price: ', oPizza.calculatePrice());
