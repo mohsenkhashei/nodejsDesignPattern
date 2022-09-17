ee6a41298fb829f7caa1f1fb2de65f75dde7b2d8
Mohsen <m.khasheii@gmail.com>
2022-09-18 02:47:23 +0430

    update factory

--- Factory/factory.js
+++ Factory/factory.js
@@ -1,20 +1,42 @@
-/*  
-factory design pattern 
-
-*/
-
-const Engineer = require('./engineer')
-const Manager = require('./manager')
-const Director = require('./director')
-
-const Factory = function(fname, lname, type){
-    if(type === 'engineer'){
-        return new Engineer(fname,lname)
-    } else if(type === 'manager') {
-        return new Manager(fname, lname)
-    } else if(type === 'director'){
-        return new Director(fname, lname)
-    }
+class Director {
+	constructor(fname, lname) {
+		this.firstName = fname;
+		this.lastName = lname;
+	}
+	getName() {
+		return 'Director ' + this.firstName + ' ' + this.lastName;
+	}
 }
+class Manager {
+	constructor(fname, lname) {
+		this.firstName = fname;
+		this.lastName = lname;
+	}
+	getName() {
+		return 'Manager ' + this.firstName + ' ' + this.lastName;
+	}
+}
+class Engineer {
+	constructor(fName, lName) {
+		this.firstName = fName;
+		this.lastName = lName;
+	}
+	getName() {
+		return ' Engineer ' + this.firstName + ' ' + this.lastName;
+	}
+}
+/*  
+    factory usage
+*/
+const Factory = (fname, lname, type) => {
+	if (type === 'engineer') {
+		return new Engineer(fname, lname);
+	} else if (type === 'manager') {
+		return new Manager(fname, lname);
+	} else if (type === 'director') {
+		return new Director(fname, lname);
+	}
+};
 
-module.exports = Factory
+const newFactory = Factory('Mohsen', 'Khashei', 'engineer');
+console.log(newFactory.getName());
